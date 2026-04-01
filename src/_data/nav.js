ed6afc88619f2c247b8ef3fca2243e52301f15d3
Dr Mark O'Connor <dr.mark.oconnor@googlemail.com>
2026-04-01 15:57:48 +0100

    final changes clapham

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -1,6 +1,7 @@
 export default [
 	{ label: 'Home', href: '/' },
 	{ label: 'Work', href: '/work/' },
+	{ label: 'Gigs', href: '/gigs/' },
 	{ label: 'About', href: '/about/' },
 	{ label: 'CV', href: '/cv/' },
 	{ label: 'Practice', href: '/practice/' },
